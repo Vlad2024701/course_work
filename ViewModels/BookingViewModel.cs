91af57a0320b6ed1aba1401a8fe7bc6d6bb471b4
Vlad101202 <simakovic_vladislav@mail.ru>
2021-05-22 19:12:05 +0300

    wtfNumber

--- ViewModels/BookingViewModel.cs
+++ ViewModels/BookingViewModel.cs
@@ -98,7 +98,7 @@ namespace AutoParking.ViewModels
 
 			SqlClient.GetInstance().Bookings.Add(
 				new Booking(UserManager.CurrentUser as User, Place,
-								  DateTime.Now, DateTime.Now.AddHours(Convert.ToDouble(Hours))), _carNumber);
+								  DateTime.Now, DateTime.Now.AddHours(Convert.ToDouble(Hours)), _carNumber));
 
 			SqlClient.GetInstance().SaveChanges();
 
